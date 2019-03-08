dff644525ba7736c6a7b79255cfdb96affcbe42a
code1031 <1031247267@qq.com>
2019-03-08 11:52:40 +0800

    完成首页路由配置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+
+import Login from './login';
+import Main from './module/main';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        初始化
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route path="/home" component={Main}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
