29d1752e1765aa2e94a2bc042218ec6171a7d297
code1031 <1031247267@qq.com>
2019-03-09 11:58:40 +0800

    初步完成菜单的路由配置-bug

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 // 导入组件库的样式
 import 'semantic-ui-css/semantic.min.css'
+// 导入外界字体图标
+import './assets/fonts/iconfont.css';
 
 import AuthCheck from './auth';
 import Login from './login';
@@ -21,6 +23,7 @@ class App extends Component {
           <Route exact path="/" component={Login}/>
           <Route path="/home" component={Main}/>
           <AuthCheck path='/abc' component={Abc}/>
+          <Route render={() => <div>default</div>}/>
         </Switch>
       </BrowserRouter>
     );
--- src/module/main.js
+++ src/module/main.js
@@ -1,33 +1,52 @@
 import React from 'react';
 import { Grid, Icon } from 'semantic-ui-react';
+import { Link, Route, Switch } from "react-router-dom";
+
 import './main.css';
+
+// 自定义链接样式
+function Menu(props) {
+  let {to, mname, isExact, icon} = props;
+  return (
+    <Route path={to} exact={isExact} children={({match})=>{
+      return (
+        <Link to={to}>
+          <div className={'placeholder'}>
+            <i className={'iconfont icon-all'}></i>
+            <div className={"active"}>{mname}</div>
+          </div>
+        </Link>
+      );
+    }}/>
+  );
+}
 
 class Main extends React.Component {
   render() {
     return (
       <div className='main-container'>
         <div className="main-content">
-          内容
+          <Switch>
+            <Route path='/home/main' render={()=><div>main</div>}/>
+            <Route path='/home/info' render={()=><div>info</div>}/>
+            <Route path='/home/chat' render={()=><div>chat</div>}/>
+            <Route path='/home/my' render={()=><div>my</div>}/>
+          </Switch>
         </div>
         <div className="main-menu">
           <Grid columns={4} divided>
             <Grid.Row>
               <Grid.Column>
-                <div className='menu-icon'>
-                  <Icon name='home' size='small' />
-                </div>
-                <div className='menu-name'>
-                  菜单
-                </div>
+                <Menu to='/home/main' isExact={true} icon='all' mname='主页'/>
               </Grid.Column>
               <Grid.Column>
-                菜单
+                <Menu to='/home/info' icon='all' mname='资讯'/>
               </Grid.Column>
               <Grid.Column>
-                菜单
+                <Menu to='/home/chat' icon='all' mname='微聊'/>
               </Grid.Column>
               <Grid.Column>
-                菜单
+                <Menu to='/home/my' icon='all' mname='我的'/>
               </Grid.Column>
             </Grid.Row>
           </Grid>
