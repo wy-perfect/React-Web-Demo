3953425133fb91e1ece8c4ad56248879a1313862
code1031 <1031247267@qq.com>
2019-03-11 09:20:45 +0800

    完成首页轮播图布局

--- src/module/home/index.js
+++ src/module/home/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
 import './index.css';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [
+        {
+          original: 'http://lorempixel.com/1000/600/nature/1/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/2/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+        },
+        {
+          original: 'http://lorempixel.com/1000/600/nature/3/',
+          thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className='home-container'>
         <div className='home-searchbar'>
           <Input fluid icon='search' placeholder='Search...' />
+        </div>
+        <div>
+          <ImageGallery 
+            showThumbnails={false} 
+            showPlayButton={false}
+            showFullscreenButton={false}
+            items={this.state.images} />
         </div>
       </div>
     );
