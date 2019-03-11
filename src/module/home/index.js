bcafaa4289415984d8ed5e41717bc9f88629479d
code1031 <1031247267@qq.com>
2019-03-11 09:39:45 +0800

    完成首页轮播功能数据处理

--- src/module/home/index.js
+++ src/module/home/index.js
@@ -3,12 +3,13 @@ import { Input } from 'semantic-ui-react';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 import './index.css';
+import axios from 'axios';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [
+      swipe: [
         {
           original: 'http://lorempixel.com/1000/600/nature/1/',
           thumbnail: 'http://lorempixel.com/250/150/nature/1/',
@@ -24,6 +25,16 @@ class Home extends React.Component {
       ]
     }
   }
+
+  componentDidMount() {
+    // 获取轮播图图片数据
+    axios.post('http://47.96.21.88:8086/homes/swipe').then(res=>{
+      this.setState({
+        swipe: res.data.data.list
+      });
+    });
+  }
+
   render() {
     return (
       <div className='home-container'>
@@ -35,7 +46,7 @@ class Home extends React.Component {
             showThumbnails={false} 
             showPlayButton={false}
             showFullscreenButton={false}
-            items={this.state.images} />
+            items={this.state.swipe} />
         </div>
       </div>
     );
