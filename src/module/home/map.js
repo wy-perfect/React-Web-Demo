5233f6b67b9d1904c26925abe690bf2b7160a208
code1031 <1031247267@qq.com>
2019-03-12 12:42:13 +0800

    完成地图找房基本效果

--- src/module/home/map.js
+++ src/module/home/map.js
@@ -10,6 +10,29 @@ class Mymap extends React.Component {
     history.goBack();
   }
 
+  initMap = () => {
+    // 初始化地图（在React中全局对象要通过window来访问）
+    let BMap = window.BMap;
+    // 创建Map实例
+    let map = new BMap.Map("allmap");    
+    // 初始化地图,设置中心点坐标和地图级别
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  
+    //添加地图类型控件
+    map.addControl(new BMap.MapTypeControl({
+      mapTypes:[
+          window.BMAP_NORMAL_MAP,
+          window.BMAP_HYBRID_MAP
+      ]}));   
+    // 设置地图显示的城市 此项是必须设置的
+    map.setCurrentCity("北京");   
+    // 开启鼠标滚轮缩放       
+    map.enableScrollWheelZoom(true); 
+  }
+
+  componentDidMount() {
+    this.initMap();
+  }
+
   render() {
     return (
       <div className = 'map-house' >
