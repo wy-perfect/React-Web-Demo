9e7abf9038329609d24f4b09b5980b89f527660e
code1031 <1031247267@qq.com>
2019-03-12 11:47:15 +0800

    完成图表功能展示

--- src/module/home/calculator.js
+++ src/module/home/calculator.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import { Icon, Tab, Grid, Dropdown, Input, Button } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
+import ReactEcharts from 'echarts-for-react';
+
+// 封装图表组件
+class MyChart extends React.Component {
+  getOptions = () => {
+    // 该选项来自于百度echarts官方的案例中option
+    let option = {
+      title : {
+        text: '贷款数据统计',
+        x:'center'
+      },
+      tooltip : {
+        trigger: 'item',
+        formatter: "{c}"
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['贷款总额','支付利息']
+      },
+      series : [{
+        name: '访问来源',
+        type: 'pie',
+        radius : '55%',
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'贷款总额'},
+          {value:310, name:'支付利息'}
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }]
+    };
+    return option;
+  }
+
+  render() {
+    return (
+      <ReactEcharts option={this.getOptions()}/>
+    );
+  }
+}
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -138,6 +185,9 @@ class Calculator extends React.Component {
             <Button onClick={this.handleCalc} fluid color='green'>计算</Button>
           </Grid.Column>
         </Grid.Row>
+        <div className="calc-chart">
+          <MyChart/>
+        </div>
       </Grid>
     );
     const panes = [
